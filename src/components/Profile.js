0f0fa935cc164785433fc76117daa8b0a5354330
Ankur Sharma <ankursharma1493@gmail.com>
2023-05-27 02:44:10 +0530

    added profile page layout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import "./Profile.css"
 const Profile = () => {
+
+
+  
+
   return (
-    <div>
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
-     <h1>Profile</h1> 
+    <div className='profile'>
+      <div className="profileBox">
+        <h1>Profile</h1>
+        <div className="profileDetail">
+            <img src="./images/user-profile.avif" alt="" />
+            <h3>Ankur Sharma</h3>  
+        </div>
+        <button>Log Out</button>
+      </div>
     </div>
   )
 }
